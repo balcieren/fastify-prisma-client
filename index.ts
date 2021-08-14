f29264859b250e2426d9417d0da5b522d5217ad7
Eren BALCI <erenbalci3939@gmail.com>
2021-08-14 20:21:25 +0300

    refactored

--- index.ts
+++ index.ts
@@ -3,27 +3,41 @@ import fp from "fastify-plugin";
 import { PrismaClient } from "@prisma/client";
 import { PrismaClientOptions } from "@prisma/client/runtime";
 
-const fastifyPrismaClient: FastifyPluginCallback<PrismaClientOptions> = (
-  fastify,
-  options,
-  done
-) => {
-  const prisma = new PrismaClient({
-    datasources: options?.datasources,
-    log: options?.log,
-    errorFormat: options?.errorFormat,
-    rejectOnNotFound: options?.rejectOnNotFound,
-  });
-
-  fastify.decorate("prisma", prisma).decorateRequest("prisma", prisma);
-
-  done();
+export type FastifyPrismaClientOptions = {
+  datasources: PrismaClientOptions["datasources"];
+  errorFormat: PrismaClientOptions["errorFormat"];
+  log: PrismaClientOptions["log"];
+  rejectOnNotFound: PrismaClientOptions["rejectOnNotFound"];
 };
 
-export default fp(fastifyPrismaClient, {
-  fastify: "3.x",
+const prismaClient: FastifyPluginCallback<FastifyPrismaClientOptions> = (
+  fastify,
+  options,
+  next
+) => {
+  if (fastify.prisma) {
+    return next(new Error("fastify-prisma-client has been defined before"));
+  }
+
+  const prisma = new PrismaClient(options);
+
+  fastify
+    .decorate("prisma", prisma)
+    .decorateRequest("prisma", fastify.prisma)
+    .addHook("onClose", async (_, done) => {
+      await _.prisma.$disconnect();
+      done();
+    });
+
+  return next();
+};
+
+const fastifyPrismaClient = fp(prismaClient, {
   name: "fastify-prisma-client",
+  fastify: ">=3.x",
 });
+
+export default fastifyPrismaClient;
 
 declare module "fastify" {
   interface FastifyRequest {
