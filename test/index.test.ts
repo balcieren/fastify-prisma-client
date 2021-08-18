093adbe411e7203658ccc034568243765f90bbe7
Eren BALCI <erenbalci3939@gmail.com>
2021-08-19 00:50:36 +0300

    added test

---
+++ test/index.test.ts
@@ -0,0 +1,42 @@
+import Fastify from "fastify";
+import tap from "tap";
+import fastifyPrismaClient from "../index";
+
+const buildApp = async (t: Tap.Test) => {
+  const fastify = Fastify({ logger: { level: "error" } });
+
+  t.teardown(() => {
+    fastify.close();
+  });
+
+  return fastify;
+};
+
+tap.test("fastify-prisma-client", async (t) => {
+  t.test("register plugin", async (t) => {
+    const fastify = await buildApp(t);
+
+    try {
+      await fastify.register(fastifyPrismaClient);
+      t.ok("prisma" in fastify, "should be include prisma in fastify");
+    } catch (error) {
+      console.log(error);
+      t.error(error);
+    }
+  });
+
+  t.test("generated user by prisma", async (t) => {
+    const fastify = await buildApp(t);
+
+    try {
+      await fastify.register(fastifyPrismaClient);
+      t.ok(
+        "user" in fastify.prisma,
+        "should be include user in fastify.prisma"
+      );
+    } catch (error) {
+      console.log(error);
+      t.error(error);
+    }
+  });
+});
